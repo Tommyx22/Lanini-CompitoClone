6a5248760bcf5164d86c51f257d382edd13ebe38
Tommaso_Lanini <provvisoria.lanini.tommaso@itismeucci.com>
2023-03-30 09:12:51 +0200

    inserito nome

--- stampa.cpp
+++ stampa.cpp
@@ -2,6 +2,7 @@
 Scrivere un programma che stampi "n" volte la stringa "Bada come la fuma"  con "n" dato in input dall'utente.
 
 NOME COGNOME DATA
+Tommaso Lanini 30/03/2023
 */
 
 # include <iostream>
